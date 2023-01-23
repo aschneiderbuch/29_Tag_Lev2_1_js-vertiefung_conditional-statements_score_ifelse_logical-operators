9d47439722f34070db7d14682cef18b9f12504c4
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-23 14:48:41 +0100

    testPhase

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ Gewinner ausgeben mit Punkten
 // Variablen
 let spieler1Name = document.querySelector("#spieler1Name");
 let spieler1Groesse = document.querySelector("#spieler1Groesse");
-let spieler1Alter = document.querySelector("#spieler2Alter");
+let spieler1Alter = document.querySelector("#spieler1Alter");
 
 let spieler2Name = document.querySelector("#spieler2Name");
 let spieler2Groesse = document.querySelector("#spieler2Groesse");
@@ -47,5 +47,36 @@ buttonVergleich.addEventListener("click", pruefeUnterschied);
 
 // Funktion
 function pruefeUnterschied(){
+    console.log("pruefeUnterschied function wird aufgerufen");
+    let ergebnissVergleich = 0; // = Name wer ist der Sieger
+    let ergebnissPunkteSpieler1 = 0; // = wieviel Gesamtpunkte hat Spieler 1
+    let ergebnissPunkteSpieler2 = 0; // = wieviel Gesamtpunkte hat Spieler 2                    
+
+    ergebnissPunkteSpieler1 =( parseInt(spieler1Alter.value)*5 + parseInt(spieler1Groesse.value));
+    console.log(spieler1Alter.value);
+    console.log(spieler1Groesse.value);
+    console.log(ergebnissPunkteSpieler1);
     
+    ergebnissPunkteSpieler2 = ( parseInt(spieler2Alter.value)*5 + parseInt(spieler2Groesse.value));
+    console.log(spieler2Alter.value);
+    console.log(spieler2Groesse.value);
+    console.log(ergebnissPunkteSpieler2);
+
+
+
+        if(ergebnissPunkteSpieler1 > ergebnissPunkteSpieler2){
+            ergebnissVergleich = ("Der Sieger ist " + spieler1Name.value + " und hat " + ergebnissPunkteSpieler1 + " Punkte");
+        }
+        else if(ergebnissPunkteSpieler1 < ergebnissPunkteSpieler2){
+            ergebnissVergleich = ("Der Sieger ist " + spieler2Name.value + " und hat " + ergebnissPunkteSpieler2 + " Punkte");
+        }
+        else if (ergebnissPunkteSpieler1 === ergebnissPunkteSpieler2){
+            ergebnissVergleich = ("Es ist unentschieder, weder " + spieler1Name.value + " noch " + spieler2Name.value + " hat gewonnen");
+        }
+        else {ergebnissVergleich = ("Fehler !!! Ungültig, das Spiel muss wiederholt werden.")}
+
+
+        outputFeld.innerHTML = ergebnissVergleich;
+        outputFeld.style.color = "green";
+        outputFeld.classList.add("js_backgroundColorSieger");
 }
